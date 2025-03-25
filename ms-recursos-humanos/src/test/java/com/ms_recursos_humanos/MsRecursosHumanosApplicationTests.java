6536c10731bb32b06bae414b41658add56c333b8
Renato <rnt.graca01@gmail.com>
2025-03-25 19:49:07 -0300

    criando ms-recursos-humanos

---
+++ ms-recursos-humanos/src/test/java/com/ms_recursos_humanos/MsRecursosHumanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms_recursos_humanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRecursosHumanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
