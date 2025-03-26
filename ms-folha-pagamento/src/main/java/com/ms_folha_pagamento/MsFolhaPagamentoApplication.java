42c36894e330888b3792b4b2fa429ec6e8b95883
Renato <rnt.graca01@gmail.com>
2025-03-25 21:00:48 -0300

    feat: ms-folha-pagamento criado

---
+++ ms-folha-pagamento/src/main/java/com/ms_folha_pagamento/MsFolhaPagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.ms_folha_pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFolhaPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFolhaPagamentoApplication.class, args);
+	}
+
+}
---
+++ ms-folha-pagamento/src/test/java/com/ms_folha_pagamento/MsFolhaPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms_folha_pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFolhaPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
