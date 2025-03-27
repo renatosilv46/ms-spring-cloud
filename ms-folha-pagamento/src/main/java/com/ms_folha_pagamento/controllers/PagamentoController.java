22b34932174d9792303a78f6d967c3e47151152a
Renato <rnt.graca01@gmail.com>
2025-03-27 00:19:49 -0300

    feat: Implementação do circuit breaker com resilience4j

--- ms-folha-pagamento/src/main/java/com/ms_folha_pagamento/controllers/PagamentoController.java
+++ ms-folha-pagamento/src/main/java/com/ms_folha_pagamento/controllers/PagamentoController.java
@@ -3,6 +3,7 @@ package com.ms_folha_pagamento.controllers;
 import com.ms_folha_pagamento.dto.PagamentoResponse;
 import com.ms_folha_pagamento.mappers.PagamentoMapper;
 import com.ms_folha_pagamento.services.PagamentoService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ public class PagamentoController {
     private final PagamentoService pagamentoService;
     private final PagamentoMapper pagamentoMapper;
 
+    @CircuitBreaker(name = "obterPagamentosCircuitBreaker", fallbackMethod = "obterPagamentosAlternativo")
     @GetMapping("/{idTrabalhador}/dias/{dias}")
     public ResponseEntity<PagamentoResponse> obterPagamentos(
             @PathVariable Long idTrabalhador,
@@ -26,4 +28,14 @@ public class PagamentoController {
         var response = this.pagamentoMapper.toResponse(this.pagamentoService.getPagamento(idTrabalhador, dias));
         return ResponseEntity.ok(response);
     }
+
+    public ResponseEntity<PagamentoResponse> obterPagamentosAlternativo(
+             Long idTrabalhador,
+             Integer dias,
+             Throwable t) {
+        Double total = Double.parseDouble(String.valueOf(400.0 * dias));
+        return ResponseEntity.ok(
+                new PagamentoResponse("Cliente alternativo", 400.0, dias,total));
+    }
+
 }
