b4d25668a766072a1bb74a9354d8e188127663a5
Renato <rnt.graca01@gmail.com>
2025-04-01 22:40:13 -0300

    feat: Ajuste do circuit breaker para pagamentos

--- ms-folha-pagamento/src/main/java/com/ms_folha_pagamento/controllers/PagamentoController.java
+++ ms-folha-pagamento/src/main/java/com/ms_folha_pagamento/controllers/PagamentoController.java
@@ -4,12 +4,15 @@ import com.ms_folha_pagamento.dto.PagamentoResponse;
 import com.ms_folha_pagamento.mappers.PagamentoMapper;
 import com.ms_folha_pagamento.services.PagamentoService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,22 +23,25 @@ public class PagamentoController {
     private final PagamentoMapper pagamentoMapper;
 
     @CircuitBreaker(name = "obterPagamentosCircuitBreaker", fallbackMethod = "obterPagamentosAlternativo")
+    @TimeLimiter(name = "obterPagamentosCircuitBreaker", fallbackMethod = "obterPagamentosAlternativoAsync")
     @GetMapping("/{idTrabalhador}/dias/{dias}")
-    public ResponseEntity<PagamentoResponse> obterPagamentos(
+    public CompletableFuture<ResponseEntity<PagamentoResponse>> obterPagamentos(
             @PathVariable Long idTrabalhador,
             @PathVariable Integer dias) {
-
-        var response = this.pagamentoMapper.toResponse(this.pagamentoService.getPagamento(idTrabalhador, dias));
-        return ResponseEntity.ok(response);
+        return CompletableFuture.supplyAsync(() -> {
+            var response = this.pagamentoMapper.toResponse(this.pagamentoService.getPagamento(idTrabalhador, dias));
+            return ResponseEntity.ok(response);
+        });
     }
 
-    public ResponseEntity<PagamentoResponse> obterPagamentosAlternativo(
-             Long idTrabalhador,
-             Integer dias,
-             Throwable t) {
-        Double total = Double.parseDouble(String.valueOf(400.0 * dias));
-        return ResponseEntity.ok(
-                new PagamentoResponse("Cliente alternativo", 400.0, dias,total));
+    public CompletableFuture<ResponseEntity<PagamentoResponse>> obterPagamentosAlternativoAsync(
+            Long idTrabalhador, Integer dias, Throwable t) {
+        return CompletableFuture.supplyAsync(() -> {
+            Double total = Double.parseDouble(String.valueOf(400.0 * dias));
+            return ResponseEntity.ok(
+                    new PagamentoResponse("Cliente alternativo", 400.0, dias, total)
+            );
+        });
     }
-
 }
+
