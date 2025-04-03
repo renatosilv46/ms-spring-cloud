bcc17a95fc57ad5d25470b49dd2e79eef3569855
Renato <rnt.graca01@gmail.com>
2025-04-02 21:46:07 -0300

    feat: criação do ms-usuarios

---
+++ ms-usuarios/src/test/java/com/ms/usuarios/MsUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
